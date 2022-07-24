b9f58d066f019a5ebb1a7820f350bb9cf141287d
MJ Pavithra <mjpavithra6520@gmail.com>
2022-07-24 23:01:00 +0530

    first question

---
+++ Question 01/index.js
@@ -0,0 +1,5 @@
+document.getElementById("submit-btn").addEventListener("click", function () {
+    var nameInput = document.getElementById("name").value;
+    var hometownInput = document.getElementById("hometown").value;
+    alert("Name : " + nameInput + "\nHometown : " + hometownInput);
+});
